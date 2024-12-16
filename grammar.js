5ece56d95452122f2debc6cb5b6429216f9cb242
Rana Ian <rana.ian@gmail.com>
2024-12-16 13:31:46 -0800

    -

--- grammar.js
+++ grammar.js
@@ -2,33 +2,50 @@ module.exports = grammar({
   name: "aindl",
 
   rules: {
+    // Root
     source_file: ($) => repeat($._definition),
 
     _definition: ($) => choice($.block, $.comment),
 
+    // Block Structure
     block: ($) =>
       seq(
-        $.block_identifier,
-        "{",
+        field("name", $.block_identifier),
+        field("left_brace", "{"),
         repeat(choice($.property, $.block, $.comment)),
-        "}",
+        field("right_brace", "}"),
       ),
 
     block_identifier: ($) => /[A-Z][A-Z_0-9]*/,
 
-    property: ($) => seq($.property_key, ":", $.value),
+    // Properties
+    property: ($) =>
+      seq(field("key", $.property_key), ":", field("value", $.value)),
 
     property_key: ($) => /[A-Z][A-Z_0-9]*/,
 
+    // Values
     value: ($) => choice($.string, $.number, $.array, $.block),
 
-    string: ($) => /"[^"]*"/,
+    // Leaf Nodes
+    string: ($) =>
+      seq(
+        field("quote", '"'),
+        field("content", /[^"]*/),
+        field("end_quote", '"'),
+      ),
 
     number: ($) => /\d+(\.\d+)?/,
 
+    // Array Structure
     array: ($) =>
-      seq("[", optional(seq($.value, repeat(seq(",", $.value)))), "]"),
+      seq(
+        field("left_bracket", "["),
+        optional(seq($.value, repeat(seq(",", $.value)))),
+        field("right_bracket", "]"),
+      ),
 
+    // Comments
     comment: ($) => token(seq("//", /.*/)),
   },
 });
